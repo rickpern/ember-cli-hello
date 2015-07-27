de4ba0c70a1ec27cfdc8aa57dad1033993edf709
Richard Pernikoff <rickpern@gmail.com>
2015-07-26 19:06:41 -0500

    add hello.js via bower, include hello.all.js for now, and make jshint not complain

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-hello'
+  name: 'ember-cli-hello',
+  
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/hello/dist/hello.all.js');
+  },
+
+  afterInstall: function(app) {
+    this._super.afterInstall(app);
+
+    this.insertIntoFile('.jshintrc', '    "hello",', {
+      after: '"predef": [\n'
+    });
+
+    return this.insertIntoFile('.gitignore');
+  }
 };
